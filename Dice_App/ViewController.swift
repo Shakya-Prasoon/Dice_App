6b8f47c1d1b37a139d0addab16f86191faa5a7dc
Prasoon Shakya <pras95shakya@gmail.com>
2021-04-01 23:43:00 -0700

    fixed the background margin problem

--- Dice_App/ViewController.swift
+++ Dice_App/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImageView1.image = #imageLiteral(resourceName: "DiceOne")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceSix")
     }
 
-
+    @IBAction func rollButton(_ sender: Any) {
+        let diceArr = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        diceImageView1.image = diceArr.randomElement()
+        diceImageView2.image = diceArr.randomElement()
+    }
+    
 }
 
